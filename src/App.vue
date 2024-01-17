e3ac3060a56606b732326d2fa7a33df882915fdd
RYAHE1 <rayanettis4@gmail.com>
2024-01-17 11:22:25 +0100

    label

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<template>
+  <card img="https://img.att.ovh/dragon/black.jpg" name="ShadowFang" :isActive="true" />
+  <card img="https://img.att.ovh/dragon/black.jpg" name="ShadowFang" :isActive="false" />
+  <Label label="Habitat" value="fôret" />
+  <Label label="Habitat" value="" />
+  <Label label="Habitat" />
+</template>
 
-<template></template>
+<script setup>
+import Label from "@/components/Label.vue";
+</script>
 
-<style scoped></style>
+
+
+
+
---
+++ src/components/label.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>
+        <strong>{{ label }}</strong> :
+        <span v-if="value != ''">{{ value }}</span>
+        <span v-else>{{ unknown }}</span>
+    </p>
+</template>
+
+<script setup>
+const unknown = "Inconnu";
+
+const props = defineProps({
+    label: String,
+    value: {
+        type: String,
+        default: unknown,
+    },
+});
+</script>
